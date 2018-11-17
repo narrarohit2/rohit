903f1f722db5342764066dbb62284ccdebb72177
narra rohit <40605658+narrarohit2@users.noreply.github.com>
2018-11-17 11:30:17 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -2,18 +2,18 @@
 int main()
 {
 char x;
-scanf("%s",x);
+scanf("%s",&x);
 if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u')
 {
 printf("Vowel");
 }
-else if(x=='$'||x=='@'||x=='!'||x=='%'||x=='^'||x=='&'||x=='*'||x=='('||x==')')
+else if(x=='$'||x=='@'||x=='!'||x=='%'||x=='^'||x=='&'||x=='*'||x=='('||x==')'||x=='-')
 {
 printf("Invalid");
 }
 else
 {
-printf("Consonant);
+printf("Consonant");
 }
 return 0;
 }
