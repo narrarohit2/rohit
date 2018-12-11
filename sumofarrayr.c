cc2e95221ff46792be0d2bf5738fddb45b18e8d2
narra rohit <40605658+narrarohit2@users.noreply.github.com>
2018-12-11 21:32:47 +0530

    Rename sumofarray.c to sumofarrayr.c

---
+++ sumofarrayr.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i,n,k,a[100],sum=0;
+scanf("%d",&n);
+scanf("
+for(i=1;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<k;i++)
+{
+sum=sum+a[i];
+}
+printf("%d",sum);
+return 0;
+}
