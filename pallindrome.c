c29e309ac15b8ba04133de15e5f62df466773df9
narra rohit <40605658+narrarohit2@users.noreply.github.com>
2018-12-16 13:43:19 +0530

    Create pallindrome.c

---
+++ pallindrome.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int n,reverse=0,t;
+scanf("%d",&n);
+t=n;
+while(t!==0)
+{
+reverse=reverse*10;
+reverse=reverse+t%10;
+t=t/10;
+}
+if(n==reverse)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
