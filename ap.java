cc47e6330373095b35306da46a4f5697cbe416d8
narra rohit <40605658+narrarohit2@users.noreply.github.com>
2019-05-14 16:17:58 +0530

    Create ap.java

---
+++ ap.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*
+public class main{
+public static void main(String args[]){
+int n,a,d;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+a=sc.nextInt();
+d=sc.nextInt();
+int sum=0;
+for(int i=1;i<=n;i++)
+{
+sum=sum+a;
+a=a+d;
+}
+System.out.println(sum);
+}
+}
