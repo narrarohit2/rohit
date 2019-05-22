7f7d72c6e967f120fdb5efbf22981d05ed84cf4f
narra rohit <40605658+narrarohit2@users.noreply.github.com>
2019-05-22 22:30:04 +0530

    Create stringisnumeric.java

---
+++ stringisnumeric.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class main{
+public static void main(String args[]) throws java.lang.Exception{
+Scanner s=new Scanner(System.in);
+if(s.hasNextInt()||s.hasNextFloat())
+{
+System.out.print("Yes");
+}
+else
+{
+System.out.print("No");
+}
+}
+}
