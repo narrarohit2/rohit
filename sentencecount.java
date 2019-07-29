6f0c3e1939b19f5ad7c1fcbf1976be601cef7c6f
narra rohit <40605658+narrarohit2@users.noreply.github.com>
2019-07-29 16:30:06 +0530

    Create sentencecount.java

---
+++ sentencecount.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+public class main
+{
+public static void main(String args[])
+{
+String a;
+int n;
+Scanner sc = new Scanner(System.in);
+a = sc.next();
+n=a.length();
+system.out.print(n);
+}
+}
